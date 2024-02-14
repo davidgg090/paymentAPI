4214dbc900f35a3ae3773c3f021d9473bc1f01ba
david.gomez <davidgg090@gmail.com>
2024-02-14 18:28:18 -0300

    Create Customer model and update DB connection defaults
    Introduced the Customer model in SQLAlchemy with appropriate fields and defaults. Also revised the default parameters for database connection in the connection.py file, including user, password, database name, and connection pool settings.

--- src/db/connection.py
+++ src/db/connection.py
@@ -6,17 +6,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-user = os.getenv("DB_USER") or "user"
-password = os.getenv("DB_PASSWORD") or "password"
+user = os.getenv("DB_USER") or "root"
+password = os.getenv("DB_PASSWORD") or "postgres"
 host = os.getenv("DB_HOST") or "localhost"
 port = os.getenv("DB_PORT") or "5432"
-db = os.getenv("DB_NAME") or "db"
+db = os.getenv("DB_NAME") or "payment"
 
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db}"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, pool_size=10, max_overflow=20
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
---
+++ src/db/models/customer.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Numeric, DateTime, Boolean
+
+from src.db.connection import Base
+
+
+class Customer(Base):
+    """Represents a customer.
+
+    Args:
+        id (int): The unique identifier of the customer.
+        name (str): The name of the customer.
+        email (str): The email address of the customer.
+        address (str, optional): The address of the customer.
+        is_active (bool, optional): Indicates if the customer is active.
+        created_at (datetime): The timestamp when the customer was created.
+        updated_at (datetime): The timestamp when the customer was last updated.
+    """
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False, unique=True)
+    address = Column(String)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+
+
+
